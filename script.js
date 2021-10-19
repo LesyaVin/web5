e38ef6371a04b055b0fa0af26743b80e4dbf5948
Olesya <vinogradova_ol_u@mail.ru>
2021-10-20 02:35:30 +0300

    commit

---
+++ script.js
@@ -0,0 +1,29 @@
+function multiplication() {
+	var x, y, z;
+    x = parseInt(document.getElementById('x').value);
+    y = parseInt(document.getElementById('y').value);
+
+    var z = x * y;
+
+	if (x===0 || y===0)   
+    {
+        alert('Отсутствуют значения параметров.');
+    }
+	
+    if (x<0 || y<0)
+    {
+        alert('Некорректный ввод! Введите положительные числа.'); 
+    }
+
+    if (isNaN(x)==true || isNaN(y)==true) 
+        alert('Заполните все поля.');
+	
+    else
+        document.getElementById('result').innerHTML = z;
+    
+}
+window.addEventListener('DOMContentLoaded', function (multiplication) {
+    console.log("DOM fully loaded and parsed");
+    let b = document.getElementById("result");
+    d.addEventListener("click", multiplication);
+ });
