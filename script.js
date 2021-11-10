1b034614d167fb5d78b35f2cb84a159498f86224
Vinogradova Olesya <90310862+LesyaVin@users.noreply.github.com>
2021-11-10 13:42:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,15 +8,22 @@ function multiplication() {
 	if (x===0 || y===0)   
     {
         alert('Отсутствуют значения параметров.');
+	let result = "Ошибка!";
+        document.getElementById('out').innerHTML = result;
     }
 	
     if (x<0 || y<0)
     {
         alert('Некорректный ввод! Введите положительные числа.'); 
+	let result = "Ошибка!";
+        document.getElementById('out').innerHTML = result;
     }
 
     if (isNaN(x)==true || isNaN(y)==true) 
-        alert('Заполните все поля.');
+    {    alert('Заполните все поля.');
+	let result = "Ошибка!";
+        document.getElementById('out').innerHTML = result;
+    }
 	
     else
         document.getElementById('result').innerHTML = z;
